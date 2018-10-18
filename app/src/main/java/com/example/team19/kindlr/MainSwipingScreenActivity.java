6acd121fb6115ea4effc1705fe0f0fcb491a5f55
ASzot <me@andrewszot.com>
2018-10-18 00:31:43 -0700

    Updating book swipper

--- app/src/main/java/com/example/team19/kindlr/MainSwipingScreenActivity.java
+++ app/src/main/java/com/example/team19/kindlr/MainSwipingScreenActivity.java
@@ -2,6 +2,8 @@ package com.example.team19.kindlr;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainSwipingScreenActivity extends Activity {
 
@@ -9,6 +11,14 @@ public class MainSwipingScreenActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_swiping_screen);
-    }
 
+        Button likeBtn = (Button) findViewById(R.id.like_button);
+        likeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+    
 }
