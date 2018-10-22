5df0a696104faa238f0e879b33c9e85a38b48676
jdormuth1 <jdormuth@gmail.com>
2018-10-21 18:44:12 -0700

    front end for activity post book

--- app/src/main/java/com/example/team19/kindlr/PostBookActivity.java
+++ app/src/main/java/com/example/team19/kindlr/PostBookActivity.java
@@ -1,29 +1,54 @@
 package com.example.team19.kindlr;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+
 
 public class PostBookActivity extends AppCompatActivity {
+
+    private EditText title;
+    private EditText isbn;
+    private EditText author;
+    private EditText genre;
+    private EditText number;
+    private EditText pagecount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_book);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        title = (EditText)findViewById(R.id.title);
+        isbn = (EditText) findViewById(R.id.isbn);
+        author = (EditText) findViewById(R.id.author);
+        genre = (EditText) findViewById(R.id.genre);
+        pagecount = (EditText) findViewById(R.id.pagecount);
+
+        Button postBtn = (Button) findViewById(R.id.post);
+        postBtn.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
             }
         });
+
+        Button backBtn = (Button) findViewById(R.id.back);
+        backBtn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
+
+
+
 
 }
