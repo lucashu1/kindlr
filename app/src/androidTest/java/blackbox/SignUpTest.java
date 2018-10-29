cf0bed435ee64940ea74cc971d17a1d722c8b8fd
ASzot <me@andrewszot.com>
2018-10-28 23:25:03 -0700

    Finished 2 create user tests

--- app/src/androidTest/java/blackbox/SignUpTest.java
+++ app/src/androidTest/java/blackbox/SignUpTest.java
@@ -1,5 +1,6 @@
 package blackbox;
 
+import android.support.test.espresso.NoMatchingViewException;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -7,6 +8,9 @@ import android.support.test.runner.AndroidJUnit4;
 import com.example.team19.kindlr.LoginActivity;
 import com.example.team19.kindlr.R;
 import com.example.team19.kindlr.SignupActivity;
+import com.example.team19.kindlr.UserManager;
+
+import junit.framework.AssertionFailedError;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -16,8 +20,13 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.action.ViewActions.click;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 //import android.support.test.espresso.ViewAction.typeText;
 //import android.support.test.espresso.action.ViewActions.typeText;
@@ -26,12 +35,20 @@ import static android.support.test.espresso.action.ViewActions.click;
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class SignUpTest {
+    private String testUsername;
+
     @Rule
     public ActivityTestRule<LoginActivity> activityRule = new ActivityTestRule<>(LoginActivity.class);
 
     private void toSignUp() {
         onView(withId(R.id.sign_up_button))
                 .perform(click());
+    }
+
+    @Before
+    public void initFillData() {
+        // Specify a valid string.
+        testUsername = "test_user";
     }
 
     private void testFill(String firstNameStr, String lastNameStr, String usernameStr,
@@ -62,11 +79,45 @@ public class SignUpTest {
                 .perform(click());
     }
 
+    private boolean assertMainSwipingExists() {
+        try {
+            onView(withId(R.id.like_button)).check(matches(isDisplayed()));
+            return true;
+        } catch (NoMatchingViewException e) {
+            return false;
+        }
+    }
+
     @Test
     public void testCorrectCreateUser() {
+        if (UserManager.getUserManager().doesUserExist(testUsername)) {
+            UserManager.getUserManager().deleteUser(testUsername);
+        }
+
         toSignUp();
         testFill("TestUserFirst", "TestUserLast",
-                "test_user", "thisisapassword", "Redmond",
+                testUsername, "thisisapassword", "Redmond",
                 "WA", "4254639202", "szot@usc.edu");
+
+        assertTrue(UserManager.getUserManager().doesUserExist(testUsername));
+
+        assertTrue(assertMainSwipingExists());
+
+        UserManager.getUserManager().deleteUser(testUsername);
+    }
+
+    @Test
+    public void testCreateExistingUser() {
+        UserManager.getUserManager().addUser(testUsername, "thisisapassword",
+                "John", "David", "Redmond", "WA",
+                "4254639202", "szot@usc.edu");
+
+        toSignUp();
+        testFill("TestUserFirst", "TestUserLast",
+                testUsername, "thisisapassword", "Redmond",
+                "WA", "4254639202", "szot@usc.edu");
+
+        assertFalse(assertMainSwipingExists());
+        UserManager.getUserManager().deleteUser(testUsername);
     }
 }
