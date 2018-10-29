e4c0d5511a5a9a344c725dbc315f749dad323e82
lucashu1 <lucashu@usc.edu>
2018-10-29 00:00:48 -0700

    Add 3 UserManager whitebox tests

--- app/src/androidTest/java/whitebox/TransactionManagerUnitTests.java
+++ app/src/androidTest/java/whitebox/TransactionManagerUnitTests.java
@@ -4,32 +4,27 @@ import com.example.team19.kindlr.BookManager;
 import com.example.team19.kindlr.TransactionManager;
 import com.example.team19.kindlr.UserManager;
 
+import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 
 public class TransactionManagerUnitTests {
 
-    UserManager um = UserManager.getUserManager();
-    BookManager bm = BookManager.getBookManager();
-    TransactionManager tm = TransactionManager.getTransactionManager();
-
     @BeforeClass
-    public void initManagers() {
+    public static void initManagers() {
         UserManager.getUserManager().initialize();
         BookManager.getBookManager().initialize();
         TransactionManager.getTransactionManager().initialize();
-    }
 
-    @BeforeClass
-    public void createTestUsers() {
-        um.addUser("testUser1", "", "Mr", "Test", "Los Angeles", "California", "3333333333", "test1@usc.edu");
-        um.addUser("testUser2", "", "Mrs", "Test", "Los Angeles", "California", "4444444444", "test2@usc.edu");
+        UserManager.getUserManager().addUser("testUser1", "", "Mr", "Test", "Los Angeles", "California", "3333333333", "test1@usc.edu");
+        UserManager.getUserManager().addUser("testUser2", "", "Mrs", "Test", "Los Angeles", "California", "4444444444", "test2@usc.edu");
+
     }
 
     @AfterClass
-    public void deleteTestUsers() {
-        um.deleteUser("testUser1");
-        um.deleteUser("testUser2");
+    public static void deleteTestUsers() {
+        UserManager.getUserManager().deleteUser("testUser1");
+        UserManager.getUserManager().deleteUser("testUser2");
     }
 }
--- app/src/androidTest/java/whitebox/UserManagerTest.java
+++ app/src/androidTest/java/whitebox/UserManagerTest.java
@@ -2,6 +2,9 @@ package whitebox;
 
 import com.example.team19.kindlr.UserManager;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -9,19 +12,44 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class UserManagerTest {
+
+    static String username = "andyTest";
+    static String password = "asdfasdf";
+    static String email = "szot@usc.edu";
+
     @BeforeClass
-    public static void initUserManager() {
+    public static void prepUserManagerTests() {
         UserManager.getUserManager().initialize();
+        UserManager um = UserManager.getUserManager();
+    }
+
+    @Before
+    public void createTestUser() {
+        UserManager.getUserManager().addUser(username, password, "Andrew", "Szot", "Los Angeles", "California", "123-456-7890", email);
     }
 
     @Test
-    public void testAddUser() {
-        String username = "andyTest";
-        String email = "szot@usc.edu";
+    public void testGetUser() {
         UserManager um = UserManager.getUserManager();
-        um.addUser(username, "asdf", "Andrew", "Szot", "Los Angeles", "California", "123-456-7890", email);
         assertTrue(um.doesUserExist(username));
         assertEquals(um.getUserByUsername(username).getEmail(), email);
         um.deleteUser(username);
+    }
+
+    @Test
+    public void testLoginSuccessful() {
+        UserManager um = UserManager.getUserManager();
+        assertTrue(um.attemptLogin(username, password));
+    }
+
+    @Test
+    public void testLoginUnsuccessful() {
+        UserManager um = UserManager.getUserManager();
+        assertTrue(!um.attemptLogin(username, "wrongpassword"));
+    }
+
+    @After
+    public void deleteTestUser() {
+        UserManager.getUserManager().deleteUser(username);
     }
 }
