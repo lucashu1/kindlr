97cde7e938085b58f78a9e5d6be2b4ab25afc4b6
jdormuth1 <jdormuth@gmail.com>
2018-11-05 15:34:45 -0800

    added updating user profile

--- app/src/main/java/com/example/team19/kindlr/UserManager.java
+++ app/src/main/java/com/example/team19/kindlr/UserManager.java
@@ -10,12 +10,10 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class UserManager {
 
@@ -149,6 +147,17 @@ public class UserManager {
         currentUser = u;
 
         return true;
+    }
+
+    public void updateCurrentUser(User updatedUser){
+        DatabaseReference updateRef = usersRef.child(updatedUser.getUsername());
+        Map<String, Object> userUpdates = new HashMap<>();
+        userUpdates.put(updatedUser.getUsername(), updatedUser);
+        Log.d("update",updatedUser.getCity());
+        usersRef.updateChildren(userUpdates);
+
+//        userUpdates.put("alanisawesome/nickname", "Alan The Machine");
+//        usersRef.child(username).child("username").s
     }
 
     public boolean doesUserExist(String username) {
