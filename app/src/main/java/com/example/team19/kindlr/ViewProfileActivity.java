2888068d064dfec25eed12679a6cb87237ec6674
jdormuth1 <jdormuth@gmail.com>
2018-11-12 22:02:21 -0800

    added rough ui changes

--- app/src/main/java/com/example/team19/kindlr/ViewProfileActivity.java
+++ app/src/main/java/com/example/team19/kindlr/ViewProfileActivity.java
@@ -1,11 +1,10 @@
 package com.example.team19.kindlr;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class ViewProfileActivity extends Activity {
     private User displayUser;
@@ -19,8 +18,8 @@ public class ViewProfileActivity extends Activity {
         displayUser = (User)getIntent().getSerializableExtra("DISPLAY_USER");
         isCurrentUser = (UserManager.getUserManager().getCurrentUser().getUsername().equals(displayUser.getUsername()));
 
-        TextView profileNameView = (TextView)findViewById(R.id.profile_name);
-        profileNameView.setText(displayUser.getUsername());
+//        TextView profileNameView = (TextView)findViewById(R.id.profile_name);
+//        profileNameView.setText(displayUser.getUsername());
 
         Button postBookBtn = (Button) findViewById(R.id.post_book_button);
         postBookBtn.setOnClickListener(new View.OnClickListener() {
